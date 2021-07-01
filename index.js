a7e7805c49175eda65a047587b2fcc6b743cacf4
Alec Magnet <amagnet@gmail.com>
2021-07-01 07:05:15 -0400

    commit tree 945802096fb695dd6575c1c7f1e86b4e6d91b405

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const muppet = {
   partner: 'Kermit'
 };
 
+const { muppetName, color, song, job, partner } = muppet; 
+
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,6 +26,22 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { nestedJob, nestedPartner } = nestedMuppet
+
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ')
+
+const [blackAndWhite, , black, pink, ,] = farmAnimals.split(' ')
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const [r, o, y, g, b, i, v] = colors;
+
+const [, , , , , indg, ,] = colors
 
 // Strings
 
